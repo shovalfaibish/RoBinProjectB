5a825a89edccc326eb41f50f3d7576b3259563c3
shovalfaibish <93712053+shovalfaibish@users.noreply.github.com>
2024-03-03 20:33:29 +0200

    Update navigation_inner.py

--- semantic-segmentation/robin_code/navigation_inner.py
+++ semantic-segmentation/robin_code/navigation_inner.py
@@ -1,97 +1,127 @@
-import time
-import helper as h
-import constants as const
-import math
-import os
+"""!
+@brief Version 1.0 of the Navigation module.
+Starts navigation when it gets the order to do so by the manager.
+
+This is the main file. It contains a Navigation object, that handles the navigation.
+"""
+
 import threading
-from datetime import datetime
+import time
+import os
+import mysql.connector
 from pathlib import Path
-from tools.infer import SemSeg
+from navigation_inner import Navigation
 
 
-class Navigation:
-    """
-    Instantiate a Navigation object.
-    Used to navigate along a trail in a park.
-    """
+##############################
+# Globals
+##############################
 
-    def __init__(self, lab_exp=False):
-        self.__stop_thread = False
-        self.__logfile = os.path.join(Path.home(), "/RoBin_Files/Logs/Latest/Navigation_Logs.txt")
-        self.__lab_exp = lab_exp
-        self.__semseg = SemSeg(const.CFG_FILE_PATH, self.__lab_exp)
 
-    def _write_log(self, msg):
-        # Write to file and flush to stdout
-        # with open(self.__logfile, 'a') as f:
-        #     f.write(msg + '\n')
-        print(msg, flush=True)
+logfile = os.path.join(Path.home(), "RoBin_Files/Logs/Latest/Navigation_Logs.txt")
+start_TaskID = ""
+nav = Navigation()
 
-    @staticmethod
-    def _turn(degrees):
-        # TODO: Send command to driver through SQL
-        degrees = int(degrees * const.DEGREES_FIX)
-        if abs(degrees) < const.DEGREES_THRESHOLD:
-            print(f"No adjustment: {abs(degrees)} degrees is below threshold")
-            return
 
-        if degrees > 0:
-            # gs.driver.right(degrees)
-            print(f"Adjustment: Driver, Right, {degrees} degrees")
+##############################
+# Thread Functions
+##############################
 
-        else:
-            # gs.driver.left(-degrees)
-            print(f"Adjustment: Driver, Left, {-degrees} degrees")
 
-    def _adjust_direction_by_centroid(self, centroid_x, centroid_y):
-        # Calculate image center x value
-        image_center_x = const.IMG_WIDTH // 2
+# Creates a thread to look for new tasks
+def look_for_tasks():
+    try:
+        global start_TaskID
 
-        # Calculate the angle in radians and convert to degrees
-        angle_radians = math.atan2(centroid_x - image_center_x, const.IMG_HEIGHT - centroid_y)
-        angle_degrees = math.degrees(angle_radians)
+        # Connect to SQL server
+        cnx_tasks = mysql.connector.connect(user='robin', password='robin',
+                                            host='127.0.0.1',
+                                            database='RobinDB')
+        cursor_tasks = cnx_tasks.cursor()
+        write_log("Started main task thread and connected it to SQL.")
 
-        self._turn(angle_degrees)
+        while True:
+            # ManagerStatus in ModuleJobs table notifies on new tasks
+            cursor_tasks.execute("SELECT ManagerStatus FROM modulejobs WHERE Module='Navigation'")
+            result = cursor_tasks.fetchall()
 
-    def start(self):
-        self._write_log("Started navigation process.")
-        self.__stop_thread = False
-        threading.Thread(target=self.navigate).start()
+            # Parse Navigation module line
+            if len(result) == 1:
+                for (ManagerStatus, ) in result:
+                    manager_status = ManagerStatus
+                    if manager_status == 1:
+                        # New tasks are in the NavigationTasks table
+                        cursor_tasks.execute("SELECT TaskID, Command "
+                                             "FROM navigationtasks "
+                                             "WHERE Status='NEW'")
+                        result = cursor_tasks.fetchall()
 
-    def stop(self):
-        self._write_log("Stopped navigation process.")
-        self.__stop_thread = True
+                        # Iterate new tasks
+                        if len(result) > 0:
+                            for (TaskID, Command) in result:
+                                if Command == "Start":
+                                    # TODO: START CAMERA??
+                                    nav.start()
 
-    def navigate(self):
-        if self.__lab_exp:
-            # Back up local camera dir to temporary dir
-            h.backup_dir(const.LOCAL_CAMERA_OUTPUT_PATH)
+                                    # Set StartNavigation task to RUNNING
+                                    start_TaskID = TaskID
+                                    cursor_tasks.execute("UPDATE navigationtasks "
+                                                         "SET Status='RUNNING' "
+                                                         f"WHERE TaskID='{TaskID}'")
 
-        # Start semantic segmentation
-        timestamp = datetime.now().strftime("%d-%m_%H:%M:%S")
-        h.setup_output_folders(timestamp)
-        self.__semseg.start(timestamp)
+                                elif Command == "Stop":
+                                    # TODO: STOP CAMERA??
+                                    nav.stop()
 
-        # TODO: TIME SLEEP TO MAKE SURE THERE ARE SEMSEG RESULTS?
-        print("time sleep nav")
-        time.sleep(4)
+                                    # Set StartNavigation and StartNavigation tasks to DONE
+                                    cursor_tasks.execute("UPDATE navigationtasks "
+                                                         "SET Status='DONE' "
+                                                         f"WHERE TaskID={TaskID} OR TaskID={start_TaskID}")
 
-        while not self.__stop_thread:
-            # Calculate center of mass and adjust direction based on segmentation
-            centroid = h.calculate_center_of_mass(self.__semseg.get_seg_result())
-            if centroid:
-                self._adjust_direction_by_centroid(*centroid)
+                                    # Update ModuleJobs on module stopped
+                                    cursor_tasks.execute("UPDATE modulejobs "
+                                                         "SET ManagerStatus=0, ModuleStatus=1 "
+                                                         "WHERE Module='Navigation'")
+                                    start_TaskID = ""
 
-            # Change to infinite
-            # h.gs.driver.forward(const.MOVEMENT_DISTANCE)
-            print("Driver, Forward")
+                            # Update ModuleJobs that all pending tasks have been executed
+                            cursor_tasks.execute("UPDATE modulejobs "
+                                                 "SET ManagerStatus=0, ModuleStatus=1 "
+                                                 "WHERE Module='Navigation'")
 
-            # Brief pause between movements
-            time.sleep(const.TIME_PAUSE)
+            # Commit SQL changes and sleep to avoid flooding the SQL server
+            cnx_tasks.commit()
+            time.sleep(0.1)
 
-        # Stop semantic segmentation
-        self.__semseg.stop()
+    # Errors
+    except mysql.connector.Error as err:
+        write_log(f"Something went wrong with MySQL: {err}")
 
-        if self.__lab_exp:
-            h.delete_dir(f"{const.LOCAL_CAMERA_OUTPUT_PATH}_temp")
-        # TODO: AUTOMATICALLY CREATE VIDEO?
+    except Exception as e:
+        write_log("Something else went wrong at 'look_for_tasks' :" + str(e))
+
+    finally:
+        cnx_tasks.close()
+
+
+def write_log(msg):
+    # Write to file and flush to stdout
+    with open(logfile, 'a') as f:
+        f.write(msg + '\n')
+    print(msg, flush=True)
+
+
+if __name__ == '__main__':
+    # Set the log dir
+    write_log("Navigation module starting!")
+    write_log(f"Using the log directory: {os.path.dirname(logfile)}")
+
+    # Start the task thread and wait for it
+    th = threading.Thread(target=look_for_tasks)
+    th.start()
+    th.join()  # wait until it finishes
+    # TODO should I check if it finished bc it crashed?
+
+    # cnx_main.close()
+    write_log("Main closed. Bye!")
+    quit()
