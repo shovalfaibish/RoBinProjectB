061b815be3dafbb1a79e258aec547080887ad6c1
shovalfaibish <93712053+shovalfaibish@users.noreply.github.com>
2024-03-07 18:49:42 +0200

    Update Navigation.py

--- semantic-segmentation/robin_code/Navigation.py
+++ semantic-segmentation/robin_code/Navigation.py
@@ -7,9 +7,6 @@ This is the main file. It contains a Navigation object, that handles the navigat
 
 import threading
 import time
-import os
-import mysql.connector
-from pathlib import Path
 from navigation_inner import Navigation
 
 
@@ -18,8 +15,6 @@ from navigation_inner import Navigation
 ##############################
 
 
-logfile = os.path.join(Path.home(), "RoBin_Files/Logs/Latest/Navigation_Logs.txt")
-start_TaskID = ""
 nav = Navigation()
 
 
@@ -30,91 +25,78 @@ nav = Navigation()
 
 # Creates a thread to look for new tasks
 def look_for_tasks():
-    try:
-        global start_TaskID
+    # try:
+    nav.connect_to_db()
+    nav.write_log("Started look_for_tasks thread and connected to DB.")
 
-        # Connect to SQL server
-        cnx_tasks = mysql.connector.connect(user='robin', password='robin',
-                                            host='127.0.0.1',
-                                            database='RobinDB')
-        cursor_tasks = cnx_tasks.cursor()
-        write_log("Started main task thread and connected it to SQL.")
+    while True:
+        # ManagerStatus in ModuleJobs table notifies on new tasks
+        nav.tasks_cursor.execute("SELECT ManagerStatus FROM modulejobs WHERE Module='Navigation'")
+        result = nav.tasks_cursor.fetchall()
 
-        while True:
-            # ManagerStatus in ModuleJobs table notifies on new tasks
-            cursor_tasks.execute("SELECT ManagerStatus FROM modulejobs WHERE Module='Navigation'")
-            result = cursor_tasks.fetchall()
-
-            # Parse Navigation module line
-            if len(result) == 1:
-                for (ManagerStatus, ) in result:
-                    manager_status = ManagerStatus
-                    if manager_status == 1:
-                        # New tasks are in the NavigationTasks table
-                        cursor_tasks.execute("SELECT TaskID, Command "
+        # Parse Navigation module line
+        if len(result) == 1:
+            for (ManagerStatus, ) in result:
+                manager_status = ManagerStatus
+                if manager_status == 1:
+                    # New tasks are in the NavigationTasks table
+                    nav.tasks_cursor.execute("SELECT TaskID, Command "
                                              "FROM navigationtasks "
                                              "WHERE Status='NEW'")
-                        result = cursor_tasks.fetchall()
+                    result = nav.tasks_cursor.fetchall()
 
-                        # Iterate new tasks
-                        if len(result) > 0:
-                            for (TaskID, Command) in result:
-                                if Command == "Start":
-                                    # TODO: START CAMERA??
-                                    nav.start()
+                    # Iterate new tasks
+                    if len(result) > 0:
+                        for (TaskID, Command) in result:
+                            if Command == "Start":
+                                # TODO: START CAMERA??
+                                nav.start()
 
-                                    # Set StartNavigation task to RUNNING
-                                    start_TaskID = TaskID
-                                    cursor_tasks.execute("UPDATE navigationtasks "
+                                # Set Start Navigation task to RUNNING
+                                nav.start_task_id = TaskID
+                                nav.tasks_cursor.execute("UPDATE navigationtasks "
                                                          "SET Status='RUNNING' "
                                                          f"WHERE TaskID='{TaskID}'")
 
-                                elif Command == "Stop":
-                                    # TODO: STOP CAMERA??
-                                    nav.stop()
-
-                                    # Set StartNavigation and StartNavigation tasks to DONE
-                                    cursor_tasks.execute("UPDATE navigationtasks "
+                            elif Command == "Stop":
+                                # TODO: STOP CAMERA??
+                                nav.stop()
+                                # Set Start Navigation and Stop Navigation tasks to DONE
+                                nav.tasks_cursor.execute("UPDATE navigationtasks "
                                                          "SET Status='DONE' "
-                                                         f"WHERE TaskID={TaskID} OR TaskID={start_TaskID}")
+                                                         f"WHERE TaskID='{TaskID}' OR TaskID='{nav.start_task_id}'")
 
-                                    # Update ModuleJobs on module stopped
-                                    cursor_tasks.execute("UPDATE modulejobs "
-                                                         "SET ManagerStatus=0, ModuleStatus=1 "
+                                # Update ModuleJobs, and set 1st bit of ModuleStatus to 1
+                                nav.tasks_cursor.execute("UPDATE modulejobs "
+                                                         "SET ManagerStatus=0, ModuleStatus = ModuleStatus | 1 "
                                                          "WHERE Module='Navigation'")
-                                    start_TaskID = ""
 
-                            # Update ModuleJobs that all pending tasks have been executed
-                            cursor_tasks.execute("UPDATE modulejobs "
-                                                 "SET ManagerStatus=0, ModuleStatus=1 "
+                                # Reset TaskID info
+                                nav.start_task_id = ""
+                                nav.request_i = 1
+                                nav.request_task_id = ""
+
+                        # Update ModuleJobs that all pending tasks have been executed
+                        nav.tasks_cursor.execute("UPDATE modulejobs "
+                                                 "SET ManagerStatus=0, ModuleStatus = ModuleStatus | 1 "
                                                  "WHERE Module='Navigation'")
 
-            # Commit SQL changes and sleep to avoid flooding the SQL server
-            cnx_tasks.commit()
-            time.sleep(0.1)
+        # Commit SQL changes and sleep to avoid flooding the SQL server
+        nav.database.commit()
+        time.sleep(0.1)
 
-    # Errors
-    except mysql.connector.Error as err:
-        write_log(f"Something went wrong with MySQL: {err}")
-
-    except Exception as e:
-        write_log("Something else went wrong at 'look_for_tasks' :" + str(e))
-
-    finally:
-        cnx_tasks.close()
-
-
-def write_log(msg):
-    # Write to file and flush to stdout
-    with open(logfile, 'a') as f:
-        f.write(msg + '\n')
-    print(msg, flush=True)
+    # # Errors
+    # except Exception as e:
+    #     nav.write_log("Something else went wrong at 'look_for_tasks': " + str(e))
+    #
+    # finally:
+    #     nav.disconnect_from_db()
 
 
 if __name__ == '__main__':
     # Set the log dir
-    write_log("Navigation module starting!")
-    write_log(f"Using the log directory: {os.path.dirname(logfile)}")
+    nav.write_log("Navigation module starting!")
+    nav.write_log(f"Using the log directory: {nav.logfile}")
 
     # Start the task thread and wait for it
     th = threading.Thread(target=look_for_tasks)
@@ -123,5 +105,5 @@ if __name__ == '__main__':
     # TODO should I check if it finished bc it crashed?
 
     # cnx_main.close()
-    write_log("Main closed. Bye!")
+    nav.write_log("Main closed. Bye!")
     quit()
