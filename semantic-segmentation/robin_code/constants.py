e8397511830939dbee91efbb31882ef8c8d1f7ec
shovalfaibish <93712053+shovalfaibish@users.noreply.github.com>
2024-06-02 14:11:29 +0300

    Update constants.py

--- semantic-segmentation/robin_code/constants.py
+++ semantic-segmentation/robin_code/constants.py
@@ -1,5 +1,5 @@
-import cv2.aruco as aruco
 import numpy as np
+import os
 
 
 ##############################
@@ -7,10 +7,10 @@ import numpy as np
 ##############################
 
 
-ROBIN_FOLDER = r"C:/Users/user/AppData/Roaming/GroundStation/"
-PREFIX = ""
+ROBIN_FOLDER = r"/home/nvidia/RoBin_Files/"
+PREFIX = "/home/nvidia/PycharmProjects/RoBinProjectB/semantic-segmentation/"
 CAMERA_OUTPUT_PATH = f"{ROBIN_FOLDER}Images"
-LOCAL_CAMERA_OUTPUT_PATH = f"{PREFIX}data/infer_kislak"
+LOCAL_CAMERA_OUTPUT_PATH = f"{PREFIX}data/new_kislak"
 OUTPUT_FOLDERS = ["camera", "semseg", "binary"]
 TRAIN_SCRIPT_PATH = f"{PREFIX}tools/train.py"
 
@@ -35,9 +35,6 @@ IMG_WIDTH, IMG_HEIGHT = 128, 72
 VIDEO_WIDTH, VIDEO_HEIGHT = IMG_WIDTH * 3, IMG_HEIGHT
 BLACK_IMAGE = np.zeros((IMG_HEIGHT, IMG_WIDTH, 3), np.uint8)
 
-ARUCO_DICT = aruco.getPredefinedDictionary(aruco.DICT_6X6_250)
-ARUCO_PARAMS = aruco.DetectorParameters()
-
 
 ##############################
 # Movement Constants
@@ -45,12 +42,5 @@ ARUCO_PARAMS = aruco.DetectorParameters()
 
 
 TIME_PAUSE = 3
-ARUCO_TIME_PAUSE = 5
-MOVEMENT_DISTANCE = 80
 DEGREES_THRESHOLD = 4
 DEGREES_FIX = 0.48 * 0.9
-
-
-##############################
-# Training Constants
-##############################
